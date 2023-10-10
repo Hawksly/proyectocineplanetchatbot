b0d719f796e7ef93ab970540798f630287626995
Hawksly <miguelguillen24mg@gmail.com>
2023-10-10 13:55:01 -0500

    Maximizar

--- Proyecto Final/res/js/chat.js
+++ Proyecto Final/res/js/chat.js
@@ -3,19 +3,24 @@ const chatbot = document.getElementById('chatbot');
 chatButton.addEventListener('click', () => {
     chatbot.classList.toggle('active');
 });
-
 document.addEventListener("DOMContentLoaded", function () {
     const chatBody = document.querySelector(".offcanvas");
     const maximizeButton = document.getElementById("maximize-chat-button");
+    const maximizeIcon = document.getElementById("maximize-icon");
+  
     let isMaximized = false;
+  
     maximizeButton.addEventListener("click", function () {
       if (isMaximized) {
         chatBody.style.width = "500px";
+        maximizeIcon.src = "/res/images/maximizar.png";
         isMaximized = false;
       } else {
         chatBody.style.width = "1500px";
+        maximizeIcon.src = "/res/images/minimizar.png";
         isMaximized = true;
       }
     });
   });
+  
   
